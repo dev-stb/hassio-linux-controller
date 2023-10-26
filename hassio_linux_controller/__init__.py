3f86ec0e790981b3134f31768f954c56ddeda2d2
S. Bonzelet <mail@sbonzelet.de>
2023-10-26 09:13:35 +0200

    fix: :bug: init modules after log init

--- hassio_linux_controller/__init__.py
+++ hassio_linux_controller/__init__.py
@@ -5,16 +5,17 @@ import logging
 import hassio_linux_controller.env as env
 from .models import HassioError
 
-import hassio_linux_controller.display as display
-import hassio_linux_controller.power as power
-import hassio_linux_controller.firefox as firefox
-
 
 def main():
     try:
         env.load()
         logging.basicConfig(stream=sys.stdout, level=env.config.log_level)
         _logger = logging.getLogger(__name__)
+
+        import hassio_linux_controller.display as display
+        import hassio_linux_controller.power as power
+        import hassio_linux_controller.firefox as firefox
+
         display.loop_step()
         firefox.loop_step()
         while True:
