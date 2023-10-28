8d271ee7e694e9cb0517077b09249943ef79466e
Simon Bonzelet <opensource@sbonzelet.de>
2023-10-28 17:46:52 +0200

    fix: :bug: syntax

--- hassio_linux_controller/display.py
+++ hassio_linux_controller/display.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import time
- import subprocess
+import subprocess
 
 import hassio_linux_controller.env as env
 import hassio_linux_controller.api as api
@@ -11,24 +11,27 @@ _logger = logging.getLogger(__name__)
 __log_state = 0
 
 
-def set_to(on:bool):
+def set_to(on: bool):
     global __log_state
     global _logger
+    set_str = "on" if on else "off"
     if __log_state == 0:
-        _logger.info(f"Display {'on' if on else 'off'}")
+        _logger.info(f"Display {set_str}")
         __log_state = 1
     if not env.config.dry_run:
-        subprocess = subprocess.Popen(
-            f"xset -display {env.config.display} dpms force {'on' if on else 'off'}"
+        process = subprocess.Popen(
+            f"xset -display {env.config.display} dpms force {set_str}",
             stdout=subprocess.STDOUT,
             stderr=subprocess.STDOUT,
             env={"DISPLAY": env.config.display},
         )
         time.sleep(0.1)
-        exit_code = subprocess.poll()
+        exit_code = process.poll()
 
         if exit_code != 0:
-            _logger.error(f"Display {'on' if on else 'off'} failed with exit code {exit_code}")
+            _logger.error(
+                f"Display {set_str} failed with exit code {exit_code}"
+            )
 
 
 def loop_step():
